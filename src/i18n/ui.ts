3efc919638844015d805a7793128e2b4bc11c9f6
Gabriel Pereira <gabrielpedepera@gmail.com>
2023-08-15 19:59:46 +0100

    Adapt i18n for breadcrumbs

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,15 +5,17 @@ export const languages = {
 
 export const defaultLang = "en";
 
-export const ui = {
+export const ui: { [key: string]: any } = {
   en: {
     "nav.posts": "Posts",
     "nav.tags": "Tags",
     "nav.about": "About",
+    "nav.home": "Home",
   },
   "pt-BR": {
     "nav.posts": "Posts",
     "nav.tags": "Tags",
     "nav.about": "Sobre",
+    "nav.home": "Página Inicial",
   },
 } as const;
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -11,3 +11,7 @@ export function useTranslations(lang: keyof typeof ui) {
     return ui[lang][key] || ui[defaultLang][key];
   };
 }
+
+export function translateBreadcrumb(lang: keyof typeof ui, key: string) {
+  return ui[lang][`nav.${key}`] || key;
+}
